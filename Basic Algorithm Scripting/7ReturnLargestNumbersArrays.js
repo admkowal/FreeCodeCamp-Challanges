a950b28949f42b3f735d2fb9eb680462faf13787
admkowal <adm.kowal@gmail.com>
2015-09-12 21:56:48 +0200

    a

--- Basic Algorithm Scripting/7ReturnLargestNumbersArrays.js
+++ Basic Algorithm Scripting/7ReturnLargestNumbersArrays.js
@@ -1,15 +1,10 @@
 function largestOfFour(arr) {
     "use strict";
-    var i =fff 0;
-
-    sdfsdf
+    var i = 0;
     
     for (i = 0; i < arr.length; i++) {
-        var maxNumber = Math.dddmax.apply(null, arr[i]);
+        var maxNumber = Math.max.apply(null, arr[i]);
         arr[i] = maxNumber;
     }
     return arr;
-    dodaje kod 1s
 }
-
-sdfsdf
