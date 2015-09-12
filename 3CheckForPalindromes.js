c89210d6e64e9502bef5b2f8efb283027cb1d417
admkowal <adm.kowal@gmail.com>
2015-09-12 19:48:20 +0200

    palindromy dodane

---
+++ 3CheckForPalindromes.js
@@ -0,0 +1,26 @@
+function palindrome(str) {
+    "use strict";
+    var re = /(\W+)/g;
+    var check = 0;
+    
+    function trim(str) {
+        str = str.replace(re, "");
+        str = str.toLowerCase();
+        return str;
+    }
+    str = trim(str);
+
+    for (i = 0; i <= str.length - 1; i++) { 
+        if (str.charAt(i) === str.charAt(str.length - i - 1)) {
+            check = 1;
+        } else {
+            return false;
+        }
+    }   
+
+    if (check === 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
